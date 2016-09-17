22a81ea6cfba82302a1243a68babd2c7bcbb1295
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-09-17 01:24:58 -0700

    Static files being served.

--- kh_site/home/urls.py
+++ kh_site/home/urls.py
@@ -1,7 +1,9 @@
 """Urls routes for main site."""
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 
 #TODO: create view and plug here for home route
 urlpatterns = [
-    url(r'^$',  view)
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 ]
+
