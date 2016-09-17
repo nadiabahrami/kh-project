0be8d7994cea547b109a2cbbb287415e2d5db997
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-09-17 10:54:00 -0700

    Start on another page

--- kh_site/home/urls.py
+++ kh_site/home/urls.py
@@ -1,9 +1,9 @@
 """Urls routes for main site."""
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
-#TODO: create view and plug here for home route
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="home"),
 ]
 
