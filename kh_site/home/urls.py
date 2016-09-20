6102262c56bcde9191ba29d3dc0d56c66eb4cd9f
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-09-19 23:31:11 -0700

    Blog url

--- kh_site/home/urls.py
+++ kh_site/home/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
+    url(r'^blog/$', TemplateView.as_view(template_name="blog.html"), name="blog"),
     url(r'^$', TemplateView.as_view(template_name="index.html"), name="home"),
 ]
-
