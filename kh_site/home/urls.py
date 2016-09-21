cf1146d0e01c007b89c3145d8a6afedaf73fbcf7
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-09-20 19:59:29 -0700

    Getting the FAQ inorder

--- kh_site/home/urls.py
+++ kh_site/home/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^blog/$', TemplateView.as_view(template_name="blog.html"), name="blog"),
     url(r'^contact/$', TemplateView.as_view(template_name="contact.html"), name="contact"),
     url(r'^resources/$', TemplateView.as_view(template_name="resources.html"), name="resources"),
+    url(r'^faq/$', TemplateView.as_view(template_name="faq.html"), name="faq"),
     url(r'^services/$', TemplateView.as_view(template_name="services.html"), name="services"),
     url(r'^$', TemplateView.as_view(template_name="index.html"), name="home"),
 ]
