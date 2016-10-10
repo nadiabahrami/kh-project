5eb11d0bbde05735b06c17401f5f22d82a3e50c8
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-10-09 20:03:42 -0700

    reoriented blog app set up.

---
+++ kh_site/blog/urls.py
@@ -0,0 +1,7 @@
+"""Urls routes for main site."""
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="blog.html"), name="blog"),
+]
