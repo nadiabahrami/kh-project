dc2d5f210bb3348e44cb9a4b95dd77227a24c29b
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-10-10 22:51:33 -0700

    Separated out the healers route.

---
+++ kh_site/healers/urls.py
@@ -0,0 +1,8 @@
+"""Healer app urls."""
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="healers.html"), name="healers"),
+]
