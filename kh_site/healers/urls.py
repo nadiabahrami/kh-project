d9a699b518993b702336eb805966f10ee6c419e6
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-10-10 22:59:31 -0700

    Separated Healers subdomain.  closes #15

--- kh_site/healers/urls.py
+++ kh_site/healers/urls.py
@@ -4,5 +4,8 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    url(r'^play/$', TemplateView.as_view(template_name="play.html"), name="play"),
+    url(r'^dance/$', TemplateView.as_view(template_name="beyonce.html"), name="beyonce"),
+    url(r'^coaching/$', TemplateView.as_view(template_name="coaching.html"), name="coaching"),
     url(r'^$', TemplateView.as_view(template_name="healers.html"), name="healers"),
 ]
