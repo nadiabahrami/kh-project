66a6bb5e6e839ca2a4b11df59237415c47793210
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-10-11 20:54:51 -0700

    Finished address model.  closes #13

--- kh_site/healers/models.py
+++ kh_site/healers/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.forms import ModelForm
 import datetime
+from django.contrib.localflavor.us.models import USStateField
 
 
 def _image_path(instance, filename):
@@ -10,7 +11,7 @@ def _image_path(instance, filename):
 
 
 class EventManager(models.Manager):
-    """Define an Active Profile Manager class."""
+    """Define an Event Manager class."""
 
     def get_queryset(self):
         """Return a list of all active users."""
@@ -19,16 +20,27 @@ class EventManager(models.Manager):
 
 
 class Event(models.Model):
-    """Create a blog article."""
+    """Create an event."""
 
     title = models.CharField(max_length=200, blank=True)
     description = models.TextField(max_length=5000, blank=False)
     event_photo = models.ImageField(upload_to=_image_path, blank=True)
     date_published = models.DateTimeField(default=datetime.datetime.now)
     address = models.CharField(max_length=200, blank=True)
-    event_date = models.DateTimeField()
+    event_date = models.DateField()
     event_time = models.CharField(max_length=20, blank=True)
     objects = EventManager()
+
+
+class Address(models.Model):
+    """Create an address for the event."""
+
+    event = models.OneToOneField(Event, on_delete=models.CASCADE,
+                                 primary_key=True, name="address")
+    street = models.CharField(max_length=128)
+    city = models.CharField(max_length=64, default="Seattle")
+    state = USStateField(default="WA")
+    zip_code = models.CharField(max_length=5,)
 
 
 class AddEvent(ModelForm):
