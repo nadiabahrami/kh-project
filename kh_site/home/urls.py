af124d8704024b82f8a4d0a06120c05c2a48b47c
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-11-08 16:53:19 -0800

    More changes.

--- kh_site/home/urls.py
+++ kh_site/home/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^contact/$', TemplateView.as_view(template_name="contact.html"), name="contact"),
     url(r'^resources/$', TemplateView.as_view(template_name="resources.html"), name="resources"),
-    url(r'^faq/$', TemplateView.as_view(template_name="faq.html"), name="faq"),
+    # url(r'^faq/$', TemplateView.as_view(template_name="faq.html"), name="faq"),
     url(r'^services/$', TemplateView.as_view(template_name="services.html"), name="services"),
     url(r'^speaking/$', TemplateView.as_view(template_name="speaking.html"), name="speaking"),
     url(r'^$', TemplateView.as_view(template_name="index.html"), name="home"),
