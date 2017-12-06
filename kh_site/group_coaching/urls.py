b778c469dff96c3b46981e5f191e47b4e2344057
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-12-05 20:57:12 -0800

    Added the page for the next upcoming event.

--- kh_site/group_coaching/urls.py
+++ kh_site/group_coaching/urls.py
@@ -1,8 +1,10 @@
 """Url patterns for the group_coaching app."""
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from group_coaching.views import EventDetail
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="coaching.html"), name="coaching"),
+    url(r'^$', TemplateView.as_view(template_name="group_coaching/coaching.html"), name="coaching"),
+    url(r'^current$', EventDetail.as_view(), name="coaching_event"),
 ]
--- kh_site/group_coaching/views.py
+++ kh_site/group_coaching/views.py
@@ -1,5 +1,5 @@
+"""Views for group coaching."""
 from django.views.generic import DetailView
-from django.shortcuts import render
 from group_coaching.models import CoachingEvent
 
 
@@ -7,4 +7,9 @@ class EventDetail(DetailView):
     """View for displaying a single event's detail."""
 
     model = CoachingEvent
-    template = 'group_coaching/next_event.html'
+    template_name = 'group_coaching/next_event.html'
+
+    def get_object(self, queryset=None):
+        """Get the only event marked as "current"."""
+        event = CoachingEvent.objects.filter(current_event=True).first()
+        return event
