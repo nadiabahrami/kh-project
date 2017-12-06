578594dbe0f36d199edc37aad0441c8be900e58e
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-12-05 21:01:53 -0800

    Got the page for healing the healers up. Onward to the blog!

--- kh_site/healers/urls.py
+++ kh_site/healers/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     url(r'^add/', login_required(add_event), name='add_event'),
     url(r'^edit/(?P<pk>[0-9]+)/', login_required(edit_event), name='edit_event'),
     url(r'^delete/(?P<pk>[0-9]+)/', login_required(delete_event), name='delete_event'),
-    url(r'^$', TemplateView.as_view(template_name="healers.html"), name="healing_healers"),
+    url(r'^$', TemplateView.as_view(template_name="healers/healers.html"), name="healing_healers"),
 ]
