fee87c59a4f504c763fe015a25174f8780d76e62
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-12-28 14:50:19 -0800

    Mobile changes fro the about page set

--- kh_site/home/static/js/menu-ctrl.js
+++ kh_site/home/static/js/menu-ctrl.js
@@ -1,13 +1,11 @@
 $('.navbar-toggle').on('click', function(){
     if ($('#nav-mobile').hasClass('mobile-down')) {
-        console.log('going up')
         $('#nav-mobile').animate({
             top: -370
         }).removeClass('mobile-down')                
     } else {
-        console.log('going down')
         $('#nav-mobile').animate({
-            top: 80
+            top: 59
         }).addClass('mobile-down')                                
     }
 })
