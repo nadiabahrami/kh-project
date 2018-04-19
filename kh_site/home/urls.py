d8a9324744b0c4d30b89e07753e4f4ceb74b96f0
nhuntwalker <nhuntwalker@Gmail.com>
2018-04-18 21:38:51 -0700

    Changed the menu item from therapy back to counseling, and revised the home page quote

--- kh_site/home/urls.py
+++ kh_site/home/urls.py
@@ -6,6 +6,6 @@ from home.views import ContactView
 urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name="base/about.html"), name="about"),
     url(r'^contact/$', ContactView.as_view(), name="contact"),
-    url(r'^services/therapy$', TemplateView.as_view(template_name="base/therapy.html"), name="counseling"),
+    url(r'^services/counseling$', TemplateView.as_view(template_name="base/counseling.html"), name="counseling"),
     url(r'^$', TemplateView.as_view(template_name="base/index.html"), name="home"),
 ]
